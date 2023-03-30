32a70202b2168149472ac43d6ab7959d0441902b
Pham Giang Nam <namnoxus2002@gmail.com>
2023-03-30 14:47:41 +0700

    áda

---
+++ src/ql_khu_vui_choi_server/Ql_Khu_Vui_Choi_Server.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ql_khu_vui_choi_server;
+
+/**
+ *
+ * @author ACER
+ */
+public class Ql_Khu_Vui_Choi_Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
